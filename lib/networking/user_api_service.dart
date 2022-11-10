e1aba420037e6f03dcf2d7041e365c0d379b37ac
Dinesh Bala <baladnes@gmail.com>
2022-11-10 14:59:23 +0545

    Added exception handling

--- lib/networking/user_api_service.dart
+++ lib/networking/user_api_service.dart
@@ -1,22 +1,44 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:dartz/dartz.dart';
 import 'package:first_app/networking/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
+/* 1. Create a model class for the data you want to fetch from the API.
+2. Create a service class to fetch the data from the API.
+ - http request (get, post)
+ - parse string response data to model class
+ - return model
+3. Create a screen to display the data.
+  - FutureBuilder
+  - state variable
+ */
+
 class UserApiService {
-  Future<UserModel?> getUsers() async {
+  Future<Either<String, UserModel?>> getUsers() async {
     var url = "https://reqres.in/api/users";
-    var uri = Uri.parse(url);
+    var uri = Uri.parse(url).replace(
+      queryParameters: {
+        "page": "2",
+      },
+    );
 
-    var response = await http.get(uri);
+    try {
+      var response = await http.get(uri);
 
-    if (response.statusCode == 200) {
-      var responseStr = response.body;
-      var userModel = userModelFromJson(responseStr);
-      return userModel;
-    } else {
-      print("Error");
-      return null;
+      if (response.statusCode == 200) {
+        var responseStr = response.body;
+        var userModel = userModelFromJson(responseStr);
+        return Right(userModel);
+      } else {
+        return Left("Error");
+      }
+    } on SocketException catch (e) {
+      // throw Exception("No Internet Connection");
+      return Left("No Internet Connection");
+    } catch (e) {
+      rethrow;
     }
   }
 }
--- lib/networking/user_screen.dart
+++ lib/networking/user_screen.dart
@@ -5,16 +5,64 @@ import 'package:flutter/material.dart';
 
 import 'models/user_model.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  UserModel? user;
+  String? errorMessage;
+
+  @override
+  void initState() {
+    fetchUsers();
+    super.initState();
+  }
+
+  fetchUsers() async {
+    var usersData = await UserApiService().getUsers();
+
+    usersData.fold((l) {
+      setState(() {
+        errorMessage = l;
+      });
+    }, (r) {
+      setState(() {
+        user = r;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Users"),
-      ),
-      body: FutureBuilder<UserModel?>(
+        appBar: AppBar(
+          title: Text("Users"),
+        ),
+        body: errorMessage != null
+            ? Center(child: Text(errorMessage!))
+            : ListView.builder(
+                itemCount: user?.data.length,
+                itemBuilder: (context, index) {
+                  var userData = user?.data[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(userData?.avatar ??
+                          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"),
+                    ),
+                    title: Text(userData?.email ?? ""),
+                    subtitle: Text(userData?.firstName ?? ""),
+                    trailing: Text(userData?.id.toString() ?? ""),
+                  );
+                }));
+  }
+}
+
+
+/*FutureBuilder<UserModel?>(
         future: UserApiService().getUsers(),
         builder: (BuildContext context, AsyncSnapshot<UserModel?> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -52,7 +100,4 @@ class UserScreen extends StatelessWidget {
             );
           }
         },
-      ),
-    );
-  }
-}
+      ),*/
